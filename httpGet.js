641f3907399208ae3c0cc9cbbc2f9d1d592f5259
ehekatlof <pranagy@gmail.com>
2016-08-21 20:12:05 +0000

    learning node

--- httpGet.js
+++ httpGet.js
@@ -1,3 +1,7 @@
 var http = require("http");
+var entry = process.argv[2];
 
-http.get();
+http.get(entry, function(response){
+    response.setEncoding('utf8');
+    response.on('data', function(success){console.log(success)});
+});
