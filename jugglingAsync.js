0c0dd063218ad925432898000ae03d7819ecb5be
ehekatlof <pranagy@gmail.com>
2016-08-23 21:48:30 +0000

    updated spaghetti exercise

--- jugglingAsync.js
+++ jugglingAsync.js
@@ -1,20 +1,18 @@
 var http = require('http');
 var bl = require('bl');
-var urls = [process.argv[2], process.argv[3], process.argv[4]];
-var cycle = 0;
 
-function spaghetti() {
-    if (cycle < urls.length)
-    http.get(urls[cycle], function(res) {
+function spaghetti(cycle) {
+    if (cycle < process.argv.length)
+    http.get(process.argv[cycle + 2], function(res) {
         res.pipe(bl(function(err, data) {
             if (err) {
                 console.log('no m8');
             }
             console.log(data.toString());
             cycle++;
-            spaghetti();
+            spaghetti(cycle);
         }));
     });
 }
 
-spaghetti();
+spaghetti(0);
