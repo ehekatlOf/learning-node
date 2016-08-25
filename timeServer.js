9a4b976c4117c7cab90ed5c4e7166ddd58b5f347
ehekatlof <pranagy@gmail.com>
2016-08-25 00:45:10 +0000

    server update

--- timeServer.js
+++ timeServer.js
@@ -1,7 +1,16 @@
 var net = require('net');
 
-var server = net.createServer(function(socket){
-    socket.write('hello thar');
+var server = net.createServer(function(socket) {
+    var date = new Date();
+    var year = date.getFullYear().toString();
+    var month = 0+(date.getMonth()+1).toString();
+    var day = date.getDate().toString();
+    var hour = 0+date.getHours().toString();
+    var minute = date.getMinutes().toString();
+
+    socket.write("" + year + "-" + month + "-" + day + " " + hour + ":" + minute + "\r\n");
+    socket.end();
+
 });
 
-server.listen(8001);
+server.listen(process.argv[2]);
