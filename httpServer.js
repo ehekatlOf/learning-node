6260de5d9a0c6c71de3a44d14c90a6bec21bd1ae
ehekatlof <pranagy@gmail.com>
2016-08-25 19:39:08 +0000

    httpServer

--- httpServer.js
+++ httpServer.js
@@ -1,2 +1,11 @@
-var net = require('net');
+var fs = require('fs');
+var http = require('http');
 
+var server = http.createServer(function(req, res){
+
+    res.writeHead(200, {"content-type" : "text/plain"});
+
+    fs.createReadStream(process.argv[3]).pipe(res);
+});
+
+server.listen(process.argv[2]);
